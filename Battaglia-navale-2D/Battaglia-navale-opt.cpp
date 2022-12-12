cd9c964dd9d5baa6577a13d7fc52685c9e25b802
plumpkekes <113628928+plumkewe@users.noreply.github.com>
2022-12-12 14:26:22 +0100

    Ship 4 t

--- Battaglia-navale-2D/Battaglia-navale-opt.cpp
+++ Battaglia-navale-2D/Battaglia-navale-opt.cpp
@@ -7,15 +7,16 @@ using namespace std;
 
 //  Creato: 08/12/22
 //  Da: swampkewe
-//  Versione: 0.02 optimizzazione ok (poteva essere meglio ma sono pigro) , mancano i casi
+//  Versione: 0.03 ottimizzata "ok" (poteva essere meglio ma sono pigro), mancano i casi. Rinominato tutte le variabili e funzioni per capire meglio
 
-void riempiremappa (string aCopiamappa[11][11]) {
+
+void fRiempire_interfaccia (string aCopia_le_posizioni[11][11], string il_carattere) {
 	
 	for (int i = 1; i < 11; i++) {
 		
 		for (int j = 1; j < 11; j++) {
 			
-			aCopiamappa[i][j] = "⬜️";
+			aCopia_le_posizioni[i][j] = il_carattere;
 			
 		}
 		
@@ -24,7 +25,7 @@ void riempiremappa (string aCopiamappa[11][11]) {
 }
 
 
-void mostranumerini (int aTavoladagioco[11][11]){ //  una funzione tecnica, non serve per il gioco
+void mostranumerini (int aLayout_gioco[11][11]){ //  una funzione tecnica, non serve per il gioco
 	
 	cout << '\n';
 	
@@ -34,7 +35,7 @@ void mostranumerini (int aTavoladagioco[11][11]){ //  una funzione tecnica, non 
 		
 		for (int j = 1; j < 11; j++) {
 			
-			cout << aTavoladagioco[i][j] << " ";
+			cout << aLayout_gioco[i][j] << " ";
 			
 		}
 		
@@ -42,18 +43,16 @@ void mostranumerini (int aTavoladagioco[11][11]){ //  una funzione tecnica, non 
 	
 }
 
-
-
-void mostralamappa (string aCopiamappa[11][11]){
+void fMosta_linterfaccia (string aCopia_le_posizioni[11][11]){
 	
-	cout << "\n\n       A B C D E F G H I J";
+	cout << "\n\n      A B C D E F G H I J"; //  era più semplice fare così
 	
 	for (int i = 1; i < 11; i++) {
 		cout << '\n' << setw(5) << i << " ";
 		
 		for (int j = 1; j < 11; j++) {
 			
-			cout << aCopiamappa[i][j];
+			cout << aCopia_le_posizioni[i][j];
 		}
 		
 	}
@@ -62,7 +61,7 @@ void mostralamappa (string aCopiamappa[11][11]){
 	
 }
 
-void testlettera (char &iLettera) {
+void fTest_lettera_char (char &iLettera) {
 	
 	while (!(cin >> iLettera)) {
 		
@@ -74,9 +73,9 @@ void testlettera (char &iLettera) {
 	
 }
 
-void testnumero (int &intNumero) {
+void fTest_numero_int (int &sPosizione_numero_int) {
 	
-	while (!(cin >> intNumero)) {
+	while (!(cin >> sPosizione_numero_int)) {
 		
 		cout << "[!] Devi inseire un numero: ";
 		
@@ -86,11 +85,13 @@ void testnumero (int &intNumero) {
 	
 }
 
-void universalriempimento (int lunghezza_della_barca, int quante_barche, int aTavoladagioco[11][11], int iDadove, int intLettera, int intNumero, string aCopiamappa[11][11]) {
+void fInserimento_navi (int lunghezza_della_barca, int quante_barche, int aLayout_gioco[11][11], int sPosizione_lettera_int, int sPosizione_numero_int, string aCopia_le_posizioni[11][11], int sPosizione_accettata) {
 		
-	char iSelezione, iLettera;
+	char iSelezione = '0', iLettera = '0', iScelta = '0';
 	
 	for (int i = 0; i < quante_barche; ++i){
+	
+//	do {
 		
 		cout << "[=] Ora " << quante_barche << " da " << lunghezza_della_barca << " (" << i + 1 << ")";
 		
@@ -100,64 +101,76 @@ void universalriempimento (int lunghezza_della_barca, int quante_barche, int aTa
 		switch (iSelezione) {
 			case 'V': case 'v': 
 				
-				cout << "[?] Inserisci la lettera: ";
-				testlettera(iLettera);
+				//do {
 				
-				intLettera = ((int)iLettera - 96);
-				
-				cout << "[?] Da che numero vuoi inserire la nave: ";
-				testnumero(iDadove);
-				
-				while (iDadove > 11 || iDadove < 1 || aTavoladagioco[intLettera][iDadove] == 1 || aTavoladagioco[intLettera][iDadove + lunghezza_della_barca] == 1 ||  iDadove > (lunghezza_della_barca * 2) + 2) {
+					cout << "[?] Inserisci la lettera: ";
+					fTest_lettera_char(iLettera);
 					
-					cout << "[!] Posizione non valida, riprova: ";
-					testnumero(iDadove);
-				}
-				
-				for (int i = iDadove; i < iDadove + lunghezza_della_barca; i++) {
+					sPosizione_lettera_int = ((int)iLettera - 96);
 					
-					aTavoladagioco[i][intLettera] = 1;
-					aCopiamappa[i][intLettera] = "\u26F5";
-				}
-				
-				break;
-			// iDadove > 11 || iDadove < 1 || aTavoladagioco[intLettera][iDadove] == 1 ||  intLettera > (lunghezza_della_barca * 2) + 2
+					cout << "[?] Da che numero vuoi inserire la nave: ";
+					fTest_numero_int(sPosizione_numero_int);
+					
+				while (sPosizione_numero_int > 10 || sPosizione_numero_int < 1 || aLayout_gioco[sPosizione_numero_int][sPosizione_lettera_int] == 1 || aLayout_gioco[sPosizione_numero_int][sPosizione_lettera_int + 1] == 1 || sPosizione_numero_int > sPosizione_accettata) {
+						
+						cout << "[!] Posizione non valida, riprova: ";
+						fTest_numero_int(sPosizione_numero_int);
+					}
+					
+					for (int i = sPosizione_numero_int; i < sPosizione_numero_int + lunghezza_della_barca; i++) {
+						
+						aLayout_gioco[i][sPosizione_lettera_int] = 1;
+						aCopia_le_posizioni[i][sPosizione_lettera_int] = "\u26F5";
+					}
+					
+					break;
+			
+			//	} while (aLayout_gioco[(1 & 2 & 3 & 4 & 5 & 6 & 7 & 8 & 9 & 10 & 11)][sPosizione_lettera_int] == 1);
+			
+			// iDadove > 11 || iDadove < 1 || aLayout_gioco[sPosizione_lettera_int][iDadove] == 1 ||  sPosizione_lettera_int > (lunghezza_della_barca * 2) + 2
 			
 			case 'o': case 'O':
-				
-				cout << "[?] Inserisci il numero: ";
-				testnumero(intNumero);
-				
-				cout << "[?] Da che lettera vuoi inserire la nave: ";
-				testlettera(iLettera);
-				
-				intLettera = ((int)iLettera - 96);
-				
-				while (intLettera > 11 || intLettera < 1 || aTavoladagioco[intNumero][intLettera] == 1 || aTavoladagioco[intNumero][intLettera + lunghezza_della_barca] == 1 ||  intLettera > (lunghezza_della_barca * 2) + 2) {
+				//do {
 					
-					cout << "[!] Posizione non valida, riprova: ";
-					testlettera(iLettera);
+					cout << "[?] Inserisci il numero: ";
+					fTest_numero_int(sPosizione_numero_int);
 					
-					intLettera = ((int)iLettera - 96);
-				}
-				
-				for (int i = intLettera; i < intLettera + lunghezza_della_barca; i++) {
+					cout << "[?] Da che lettera vuoi inserire la nave: ";
+					fTest_lettera_char(iLettera);
 					
-					aTavoladagioco[intNumero][i] = 1;
-					aCopiamappa[intNumero][i] = "\u26F5";
-				}
+					sPosizione_lettera_int = ((int)iLettera - 96);
+					
+					while (sPosizione_lettera_int > 10 || sPosizione_lettera_int < 1 || aLayout_gioco[sPosizione_numero_int][sPosizione_lettera_int] == 1 || aLayout_gioco[sPosizione_numero_int][sPosizione_lettera_int + 1] == 1 || sPosizione_lettera_int > sPosizione_accettata) {
+						
+						cout << "[!] Posizione non valida, riprova: ";
+						fTest_lettera_char(iLettera);
+						
+						sPosizione_lettera_int = ((int)iLettera - 96);
+					}
+					
+					for (int i = sPosizione_lettera_int; i < sPosizione_lettera_int + lunghezza_della_barca; i++) {
+						
+						aLayout_gioco[sPosizione_numero_int][i] = 1;
+						aCopia_le_posizioni[sPosizione_numero_int][i] = "\u26F5";
+					}
 				
+					break;
 				
-				break;
+					//} while (aLayout_gioco[sPosizione_lettera_int][(1 & 2 & 3 & 4 & 5 & 6 & 7 & 8 & 9 & 10 & 11)] == 1);
 			
-			default: 
-				
+			default:
 				break;
-			
-		
+				
+//				cout << "\n[!] Digita 's' se vuoi giocare 'n' se no: ";
+//				cin >> iScelta;
+//				
+//				if (iScelta != 's' || iScelta != 'S')
+				
 		}
 		
-	mostralamappa(aCopiamappa);
+//	} while (iScelta == 's' || iScelta == 'S');
+		
+	fMosta_linterfaccia(aCopia_le_posizioni);
 	
 	}
 	
@@ -165,46 +178,45 @@ void universalriempimento (int lunghezza_della_barca, int quante_barche, int aTa
 }
 
 
-void ilgioco (int aTavoladagioco[11][11], int intNumero, string aCopiamappa[11][11], int iContatoreAttacco, int iPosizioneNumero, char iPosizioneLettera, int intIposizioneLettera) {
-	
+void fGioco_user (int aLayout_gioco[11][11], int sPosizione_numero_int, string aCopia_le_posizioni[11][11], int sContatore_navi_abbattute, int iPosizioneNumero, char iPosizioneLettera, int intIposizioneLettera) {
 	
 	cout << "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
-	cout << "[!] Questo è il layout, dovrai indovinare 26 posizioni nelle quali il tuo avversario ha posizionato le seguenti barche: ";
+	cout << "[!] Questo è il layout, dovrai indovinare 26 posizioni nelle quali il tuo avversario ha posizionato le seguenti navi:\n\t2:3 3:2 2:4 1:6";
 	
-	cout << "[!] Hai a disposizione soltanto 40 mosse! Non sprecarle!\n\t2:3 3:2 2:4 1:6";
+	cout << "[!] Hai a disposizione soltanto 40 mosse! Non sprecarle!";
 	
 	for (int i = 0; i < 40; i++) {
 		
-		mostralamappa(aCopiamappa);
+	fMosta_linterfaccia(aCopia_le_posizioni);
 		
 		cout << "\n[+] Questo è il tuo (" << i + 1 << ") tentativo";
 		
 		
-		cout << "[A] Inserisci il numero: ";
+		cout << "\n[A] Inserisci il numero: ";
 		cin >> iPosizioneNumero;
 		
-		cout << "[A] Inserisci la lettera: ";
+		cout << "\n[A] Inserisci la lettera: ";
 		cin >> iPosizioneLettera;
 		intIposizioneLettera = ((int)iPosizioneLettera - 96);
 		
-		if (aTavoladagioco[iPosizioneNumero][intIposizioneLettera] == 1) {
+		if (aLayout_gioco[iPosizioneNumero][intIposizioneLettera] == 1) {
 			
-			iContatoreAttacco ++;
+			sContatore_navi_abbattute ++;
 			cout << "Complimenti hai colpito la barca!";
-			aTavoladagioco[iPosizioneNumero][intIposizioneLettera] = 2;
-			aCopiamappa[iPosizioneNumero][intIposizioneLettera] = "\U0001f4a3";
-			
+			aLayout_gioco[iPosizioneNumero][intIposizioneLettera] = 2;
+			aCopia_le_posizioni[iPosizioneNumero][intIposizioneLettera] = "\U0001f4a3";
 			
 		} else {
 			
 			cout << "Purtroppo non hai colpito la barca!";
-			aTavoladagioco[iPosizioneNumero][intIposizioneLettera] = 2;
-			aCopiamappa[iPosizioneNumero][intIposizioneLettera] = "\U0001f30a";
+			aLayout_gioco[iPosizioneNumero][intIposizioneLettera] = 2;
+			aCopia_le_posizioni[iPosizioneNumero][intIposizioneLettera] = "\U0001f30a";
+			
 		}
 		
-		if (iContatoreAttacco == 26) {
+		if (sContatore_navi_abbattute == 26) {
 			
-			cout << "[&] Congrutulazioni hai vinto!";
+			cout << "\n[&] Congrutulazioni hai vinto!";
 			
 		}
 		
@@ -216,48 +228,35 @@ void ilgioco (int aTavoladagioco[11][11], int intNumero, string aCopiamappa[11][
 int main() {
 	setlocale(LC_ALL, "italian");
 	
-	int aTavoladagioco[11][11],
-	iDadove = 0, intLettera = 0, intNumero = 0;
-	string aCopiamappa[11][11];
+	int aLayout_gioco[11][11]{},
+	sPosizione_lettera_int = 0, sPosizione_numero_int = 0;
+	string aCopia_le_posizioni[11][11];
 	
 	char iSelezione, iLettera;
 	
-	//  per il attacco 
+	//  =========== Attacco da parte del giocatore
 	
-	int iPosizioneNumero = 0, intIposizioneLettera = 0, iContatoreAttacco = 0;
+	int iPosizioneNumero = 0, intIposizioneLettera = 0, sContatore_navi_abbattute = 0;
 	char iPosizioneLettera;
-	
 
-	for (int i = 0; i < 11; i++) {
-		
-		for (int j = 0; j < 11; j++) {
-			
-			aTavoladagioco[i][j] = 0;
-			
-		}
-		
-	}
+	//  =========== Preparazzione gioco
 	
-	riempiremappa(aCopiamappa);
+	fRiempire_interfaccia(aCopia_le_posizioni, "\u2B1C"); //  interfaccia pronta per l'input delle navi
 	
 	cout << "[=] Questo è layout sul quale dovrai posizionare le barche!";
-	
-	mostralamappa(aCopiamappa);
-	
-	
-	cout << "[=] Ne dovrai posizionare:\n" <<
-			"\t2:3 3:2 2:4 1:6\n\n";
+	fMosta_linterfaccia(aCopia_le_posizioni);
+	cout << "[=] Ne dovrai posizionare:\n\t2:3 3:2 2:4 1:6\n\n";
 			
-	universalriempimento(6, 1, aTavoladagioco, iDadove, intLettera, intNumero, aCopiamappa);
-	universalriempimento(4, 2, aTavoladagioco, iDadove, intLettera, intNumero, aCopiamappa);
-	universalriempimento(3, 2, aTavoladagioco, iDadove, intLettera, intNumero, aCopiamappa);
-	universalriempimento(2, 3, aTavoladagioco, iDadove, intLettera, intNumero, aCopiamappa);
-	
-	mostralamappa(aCopiamappa);
-	riempiremappa(aCopiamappa);
-	
-	ilgioco(aTavoladagioco, intNumero, aCopiamappa, iContatoreAttacco, iPosizioneNumero, iPosizioneLettera, intIposizioneLettera);
+	fInserimento_navi(6, 1, aLayout_gioco, sPosizione_lettera_int, sPosizione_numero_int, aCopia_le_posizioni, 6);
+	fInserimento_navi(4, 2, aLayout_gioco, sPosizione_lettera_int, sPosizione_numero_int, aCopia_le_posizioni, 7);
+	fInserimento_navi(3, 2, aLayout_gioco, sPosizione_lettera_int, sPosizione_numero_int, aCopia_le_posizioni, 8);
+	fInserimento_navi(2, 3, aLayout_gioco, sPosizione_lettera_int, sPosizione_numero_int, aCopia_le_posizioni, 9);
 
+	//  =========== il gioco
+	
+	fRiempire_interfaccia(aCopia_le_posizioni, "\u2B1C"); //  azera linterfaccia
+	
+	fGioco_user(aLayout_gioco, sPosizione_numero_int, aCopia_le_posizioni, sContatore_navi_abbattute, iPosizioneNumero, iPosizioneLettera, intIposizioneLettera);
 
 	cout << "[!] Hai perso perchè hai finito i tentativi, misdispiace!";
 	
